fdbcf08ed2c297bb4856c215032caa55ab865eaf
Luis Miranda <lmirandad@pucp.pe>
2017-02-08 09:05:07 -0500

    Commit 08/02

--- Iso8583Decoder/com/Iso8583Decoder/pkg/BuildIsoMessage.java
+++ Iso8583Decoder/com/Iso8583Decoder/pkg/BuildIsoMessage.java
@@ -15,7 +15,7 @@ public class BuildIsoMessage {
 	
 	public static void main(String[] args) throws IOException, ISOException {
 		//Create Packager based on XML that contain DE type
-        GenericPackager packager = new GenericPackager("Iso8583Decoder/base24.xml");
+        GenericPackager packager = new GenericPackager("Iso8583Decoder/base1.xml");
 		
         // Create ISO Message
         ISOMsg isoMsg = new ISOMsg();
@@ -28,17 +28,17 @@ public class BuildIsoMessage {
 		isoMsg.set(11, "123456");
 		isoMsg.set(12, "789101");
 		isoMsg.set(13, "989898");
-		isoMsg.set(39, "00");
+		isoMsg.set(39, "00");*/
 		isoMsg.set(41,"80117712");
-		isoMsg.set(42,"341198052      ");
+		/*isoMsg.set(42,"341198052      ");
 		isoMsg.set(44,"EB4E53397F3FE12C8BD41");*/
-		isoMsg.set(55,"82020800950580800000009A031610185F3401009F100706010C03A000009F2608AA84D42F73E9A8049F2701809F34033F00009F3602013F9F3704990FC066");
+		//isoMsg.set(55,"82020800950580800000009A031610185F3401009F100706010C03A000009F2608AA84D42F73E9A8049F2701809F34033F00009F3602013F9F3704990FC066");
 		
 		//Setting package
 		isoMsg.setPackager(packager);
 		
         //Printing the DE list
-        logISOMsg(isoMsg);
+        //logISOMsg(isoMsg);
        
         
         // Get and print the output result
@@ -49,7 +49,7 @@ public class BuildIsoMessage {
         /*for(int i=1;i<data.length;i++){
         	System.out.print(data[i]);
         }*/
-        System.out.println(dataString);
+        //System.out.println(dataString);
         
 
       
